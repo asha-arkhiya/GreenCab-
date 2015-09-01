8b3a98eebe4a6eb75bc91c31c4f471dddc4247b7
asha-arkhiya <asha.parkhiya16@gmail.com>
2015-09-01 23:37:42 +0530

    Update custom_config.js

--- custom_config.js
+++ custom_config.js
@@ -3,9 +3,9 @@
  * Created by James Parmee Morris (c) 2014-2015 all rights reserved
  * Created for iWebCab.com service usage only.
  */
-var api_key = "5c89cc5dcdbe0fdb64b3378f425753ec";
+var api_key = "631ac0deacf309d3cf00662bac04fdc6";
 var brand_name = "Green Cab";
-var brand_location = "California, USA";
+var brand_location = "Arkansas, US";
 var dispatch_number = "+14799664111";
 
 // Specific Firm Requirements
